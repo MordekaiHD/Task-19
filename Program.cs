d1fba7c73870ef481630a19067563c9ab83e5d71
Mordekai_HD <evgeni.kap@list.ru>
2023-07-12 22:10:47 +0300

    Task-19

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@
 // 12821 -> да
 // 23432 -> да
 
-void ShowPalindrome(int number1, int number2 = 0, int number3 = 0)
+void ShowPalindrome(int number1, int number2 = 0, int number3 = 0)                   
 {
     if(number2 == number3)
-        number2 = number1 / 10000;
-        number3 = number1 % 10;
+       number2 = number1 / 10000;
+       number3 = number1 % 10;
     if(number2 == number3)
-        Console.Write("Да");
+       Console.Write("Да");      
     else
         Console.Write("Нет");       
 }   
@@ -20,6 +20,4 @@ int GetInput(string text)
     return Convert.ToInt32(Console.ReadLine());
 }
 int number1 = GetInput("Введите пятизначное число: ");
-int number2 = 0;
-int number3 = 0;
 ShowPalindrome(number1);
